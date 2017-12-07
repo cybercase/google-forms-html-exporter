b87f9d360dc3b35f45487b787187c985abe8cde4
Stefano Brilli <stefanobrilli@gmail.com>
2017-12-07 11:42:52 +0100

    Add instructions for make the script working fine

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -192,6 +192,10 @@ let bootstrapForm = Handlebars.compile(`
 
 let bootstrapJs = Handlebars.compile(`
 // This script requires jQuery and jquery-form plugin
+// You can use these ones from Cloudflare CDN:
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js" integrity="sha256-2Pjr1OlpZMY6qesJM68t2v39t+lMLvxwpa8QlRjJroA=" crossorigin="anonymous"></script>
+//
 $('#bootstrapForm').submit(function (event) {
     event.preventDefault()
     var extraData = {}
@@ -236,6 +240,8 @@ $('#bootstrapForm').submit(function (event) {
         error: function () {
             // Google Docs won't allow reading the response because of CORS, so this is handled as a failure.
             alert('Form Submitted. Thanks.')
+            // You can also redirect the user to a custom thank-you page:
+            // window.location = 'http://www.mydomain.com/thankyoupage.html'
         }
     })
 })
