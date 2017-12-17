f37151ba649a78d30f2f844f3531d5ad5765ff53
Stefano Brilli <stefanobrilli@gmail.com>
2017-12-17 11:09:56 +0100

    Make forms work with google authentication
    Use ajax GET instead of POST to send auth cookies to google form.
    Fix #3

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -259,8 +259,10 @@ $('#bootstrapForm').submit(function (event) {
 
     $('#bootstrapForm').ajaxSubmit({
         data: extraData,
+        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
         error: function () {
-            // Google Docs won't allow reading the response because of CORS, so this is handled as a failure.
+            // Submit of form should be successful but JSONP callback will fail because Google Forms
+            // does not support it, so this is handled as a failure.
             alert('Form Submitted. Thanks.')
             // You can also redirect the user to a custom thank-you page:
             // window.location = 'http://www.mydomain.com/thankyoupage.html'
